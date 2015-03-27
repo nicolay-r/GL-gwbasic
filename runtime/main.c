bd45b2a028ea2bd5a41b5e03c7bf91246e6f3cc1
Nicolay Rusnachenko <kolyarus@yandex.ru>
2015-03-27 16:35:44 +0300

    Теперь парсер применяется к строке в памяти, а не к файловым объектам

--- runtime/main.c
+++ runtime/main.c
@@ -5,16 +5,23 @@
 #include "inc/handlers.h"	/* gwbh_* functions */
 #include "inc/env.h"		/* gwbe_* functions, GWBE_* types */
 
+#define GWBR_LINE_LENGTH	255
+
 int main(int argc, char** argv)
 {
 	/*
 		Prepare runtime environment
 	*/
+	char* buffer = (char*) malloc(sizeof(GWBR_LINE_LENGTH));	
 	GWBE_Environment* env = gwbe_Create();
 	/* 
 		Parse user request 
 	*/
-	Interpreter* interpreter = gwbr_Parse(); 
+	char** bufferPtr = &buffer;
+	size_t nbytes = GWBR_LINE_LENGTH - 1;
+	getline(&buffer, &nbytes, stdin);
+
+	Interpreter* interpreter = gwbr_Parse(buffer); 
 	/*
 		Handle the received "Interpreter*" 
 	*/
--- runtime/runtime.c
+++ runtime/runtime.c
@@ -3,16 +3,23 @@
 #include "../parser/basic.tab.h"
 #include "../parser/ast/interp.h"
 
+typedef struct yy_buffer_state * YY_BUFFER_STATE;
+extern YY_BUFFER_STATE yy_scan_string(char *str);
+extern void yy_delete_buffer(YY_BUFFER_STATE buffer);
 /*
 	Parse and Build AST for GWBasic Command/Statement/Function
 */
-Interpreter* gwbr_Parse()
+Interpreter* gwbr_Parse(char* toParse)
 {
 	Interpreter** interpreter = (Interpreter**) malloc (sizeof(Interpreter*));
 
+	// вынести этот код в bison.y, чтобы избавиться от 
+	// extern объявлений в этом коде	
+	YY_BUFFER_STATE buffer = yy_scan_string(toParse);
 	yyparse(interpreter);
+	yy_delete_buffer(buffer);
+	
 	Interpreter* result = *interpreter;
-
 	free(interpreter);
 	return result;
 }
