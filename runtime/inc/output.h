de2e05e9bc87e9080e0bbe7e56f6f26fa09b9142
Nicolay Rusnachenko <kolyarus@yandex.ru>
2015-04-17 16:24:24 +0300

    Добавлен заголовочный файл для вывода информации в Runtime

---
+++ runtime/inc/output.h
@@ -0,0 +1,12 @@
+/* GWBasic Runtime Header for data output */
+
+#ifndef _GWBR_OUTPUT_H_
+#define _GWBR_OUTPUT_H_
+
+/*
+	Prototypes
+*/
+void gwbo_DisplayMessage(char* msg);
+void gwbo_DisplayResult(GWBR_Result *result);
+
+#endif
