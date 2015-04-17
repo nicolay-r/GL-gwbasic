6c741d2ddc440fd59414a7b369975a659f99b682
Nicolay Rusnachenko <kolyarus@yandex.ru>
2015-04-17 16:07:32 +0300

    Переформатирован файл core.h

--- runtime/inc/core.h
+++ runtime/inc/core.h
@@ -3,44 +3,49 @@
 #ifndef _GWBR_CORE_H_
 #define _GWBR_CORE_H_
 
-/*
-	Values
-*/
-typedef union {
-	int int_val;		/* integer */
-	float float_val;	/* single prec */
-	double double_val;	/* double prec */	
-	char* str;		/* string */
-} GWBC_Value;
-
-/*
-	Array
-*/
-typedef struct GWBC_ArrayDimension {
-	int length;
-	union {
-		GWBC_Value* val;	
-		struct GWBC_ArrayDimension* next_dim;
-	}* cells;	/* cells[length] */
-} GWBC_ArrayDimension;
-
-typedef struct {
-	int elem_type;
-	int dim_count;
-	GWBC_ArrayDimension* dim; 
-} GWBC_Array; 
+typedef union GWBC_Value GWBC_Value;
+typedef struct GWBC_ArrayDimension GWBC_ArrayDimension;    
+typedef struct GWBC_Array GWBC_Array;
+typedef struct GWBC_Variable GWBC_Variable;
 
 /*
 	Variable
 */
-typedef struct {
+struct GWBC_Variable{
 	int type;
 	char* name;
 	union {
 		GWBC_Value* vals;
 		GWBC_Array* arr;
 	};
-} GWBC_Variable;
+};
+
+/*
+	Array
+*/
+struct GWBC_Array{
+	int elem_type;
+	int dim_count;
+	GWBC_ArrayDimension* dim; 
+};
+ 
+struct GWBC_ArrayDimension {
+	int length;
+	union {
+		GWBC_Value* val;	
+		struct GWBC_ArrayDimension* next_dim;
+	}* cells;	/* cells[length] */
+};
+
+/*
+	Values
+*/
+union GWBC_Value{
+	int int_val;		/* integer */
+	float float_val;	/* single prec */
+	double double_val;	/* double prec */	
+	char* str;		/* string */
+};
 
 /*
 	Functions
