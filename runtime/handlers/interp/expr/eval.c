01dd33dbfd54fbf65e43a69c0a3faa945801f37b
Nicolay Rusnachenko <kolyarus@yandex.ru>
2015-05-02 10:20:43 +0300

    Добавлены обработчики для Numeric выражений gwbasic

--- runtime/handlers/interp/expr/eval.c
+++ runtime/handlers/interp/expr/eval.c
@@ -13,7 +13,7 @@ GWBR_ExpressionResult gwbr_EvaluateExpression(GWBE_Environment* env, GWBN_Expres
 			result = gwbr_EvaluateStringExpression(env, node->str_expr);
 			break;
 		case GWBNT_NUMERICEXPRESSION:
-			/* Evaluate NumericExpression */
+			result = gwbr_EvaluateNumericExpression(env, node->num_expr);
 			break;
 	}
 	return result;
@@ -21,17 +21,205 @@ GWBR_ExpressionResult gwbr_EvaluateExpression(GWBE_Environment* env, GWBN_Expres
 
 GWBR_ExpressionResult gwbr_EvaluateNumericExpression(GWBE_Environment* env, GWBN_NumericExpression* node)
 {
-	/* Not implemented */
+	GWBR_ExpressionResult result;
+	
+	assert(node != NULL);
+
+	switch (node->type)
+	{
+		case GWBNT_ARITHMETICOPERATOR:
+			result = gwbr_EvaluateArithmeticOperator(env, node->arithm);
+			break;
+		case GWBNT_RELATIONALOPERATOR:
+			result = gwbr_EvaluateRelationalOperator(env, node->rel);
+			break;
+		case GWBNT_LOGICALOPERATOR:
+			result = gwbr_EvaluateLogicalOperator(env, node->log);
+			break;
+		case GWBNT_FUNCTIONALOPERATOR:
+			result = gwbr_EvaluateFunctionalOperator(env, node->func);
+			break;
+	}
+	return result;
+}
+
+
+GWBR_ExpressionResult gwbr_EvaluateArithmeticOperator(GWBE_Environment* env, GWBN_ArithmeticOperator *node)
+{
+	GWBR_ExpressionResult result;
+
+	assert(node != NULL);
+
+	switch (node->type)
+	{
+		case GWBBT_ADD:
+		{
+			GWBR_ExpressionResult a = gwbr_EvaluateNumericExpression(env, node->a);
+			GWBR_ExpressionResult b = gwbr_EvaluateNumericExpression(env, node->b);
+			/* выполнить сложение */
+			break;
+		}
+		case GWBBT_SUB:
+		{
+			GWBR_ExpressionResult a = gwbr_EvaluateNumericExpression(env, node->a);
+			GWBR_ExpressionResult b = gwbr_EvaluateNumericExpression(env, node->b);
+			/* выполнить вычитание */
+			break;
+		}
+		case GWBBT_MUL:
+		{
+			GWBR_ExpressionResult a = gwbr_EvaluateNumericExpression(env, node->a);
+			GWBR_ExpressionResult b = gwbr_EvaluateNumericTerm(env, node->term_b);
+			/* выполнить умножение */
+			break;
+		}
+		case GWBBT_DIV:
+		{
+			GWBR_ExpressionResult a = gwbr_EvaluateNumericExpression(env, node->a);
+			GWBR_ExpressionResult b = gwbr_EvaluateNumericTerm(env, node->term_b);
+			/* выполнить деление */
+			break;
+		}
+	}
+
+	return result;
+}
+
+GWBR_ExpressionResult gwbr_EvaluateNumericTerm(GWBE_Environment* env, GWBN_NumericTerm* node)
+{
+	GWBR_ExpressionResult result;
+
+	assert(node != NULL);
+
+	switch (node->type)
+	{
+		case GWBNT_NUMERICEXPRESSION:
+			result = gwbr_EvaluateNumericExpression(env, node->num_expr);
+			break;
+		case GWBBT_UNARY_MINUS:
+			result = gwbr_EvaluateNumericTerm(env, node->term);
+			/* домножить на унарный минус */
+			break;
+		case GWBBT_POW:
+		{
+			GWBR_ExpressionResult a = gwbr_EvaluateNumericTerm(env, node->a);
+			GWBR_ExpressionResult b = gwbr_EvaluateNumericTerm(env, node->b);
+			
+			break;
+		}
+		case GWBNT_NUMERICVARIABLE:
+			result = gwbr_EvaluateNumericVariable(env, node->var);
+			break;
+		case GWBNT_NUMERICCONSTANT:
+			result = gwbr_EvaluateNumericConstant(env, node->num_const);
+			break;
+	}
+
+	return result;
+}
+
+GWBR_ExpressionResult gwbr_EvaluateNumericVariable(GWBE_Environment *env, GWBN_NumericVariable *node)
+{
+	GWBR_ExpressionResult result;
+	
+	assert(node != NULL);
+
+	GWBC_Variable* var = gwbe_Context_GetVariable(env, node->name);
+	
+	if (var != NULL)
+	{
+		switch (var->type)
+		{
+			case GWBCT_VALUE:
+			{
+				result.val_type = var->val->type;
+				switch(var->val->type)
+				{
+					case GWBCT_INTEGER:
+					{
+						result.val.int_val = var->val->int_val;
+						break;
+					}
+
+					case GWBCT_SINGLE:
+					{
+						result.val.single_val = var->val->single_val;
+						break;
+					}
+
+					case GWBCT_DOUBLE:
+					{
+						result.val.double_val = var->val->single_val;
+						break;
+					}
+				}
+			}
+			case GWBCT_ARRAY:
+				/* Not supported */
+				break;
+		}
+	}
+	else 
+	{
+		/* Variable wasn't founded */
+	}
+
+	return result;
+}
+
+GWBR_ExpressionResult gwbr_EvaluateNumericConstant(GWBE_Environment *env, GWBN_NumericConstant *node)
+{
+	GWBR_ExpressionResult result;
+	
+	assert(node != NULL);
+
+	switch (node->type)
+	{
+		case GWBBT_INTEGER:
+			result.val_type = GWBCT_INTEGER;
+			result.val.int_val = node->const_int;
+			break;
+		case GWBCT_SINGLE:
+			result.val_type = GWBCT_SINGLE;
+			result.val.single_val = node->const_float;
+			break;
+		case GWBCT_DOUBLE:
+			/* Not Implemented */
+			break;
+	}
+
+	return result;
+}
+
+
+GWBR_ExpressionResult gwbr_EvaluateRelationalOperator(GWBE_Environment *env, GWBN_RelationalOperator *node)
+{
+	GWBR_ExpressionResult result;
+	return result;
+}
+GWBR_ExpressionResult gwbr_EvaluateLogicalOperator(GWBE_Environment *env, GWBN_LogicalOperator *node)
+{
+	GWBR_ExpressionResult result;
+	return result;
+}
+GWBR_ExpressionResult gwbr_EvaluateFunctionalOperator(GWBE_Environment *env, GWBN_FunctionalOperator *node)
+{
+	GWBR_ExpressionResult result;
+	return result;
 }
 
 GWBR_ExpressionResult gwbr_EvaluateStringExpression(GWBE_Environment* env, GWBN_StringExpression* node)
 {
+	assert(node != NULL);
 	return gwbr_EvaluateStringOperator(env, node->op);
 }
 
 GWBR_ExpressionResult gwbr_EvaluateStringOperator(GWBE_Environment* env, GWBN_StringOperator* node)
 {
 	GWBR_ExpressionResult result;
+	
+	assert(node != NULL);
+
 	switch (node->type)
 	{
 		case GWBBT_ADD:
--- runtime/handlers/interp/expr/inc/eval.h
+++ runtime/handlers/interp/expr/inc/eval.h
@@ -10,7 +10,7 @@ typedef struct GWBR_ExpressionResult GWBR_ExpressionResult;
 	Structures
 */
 struct GWBR_ExpressionResult {
-	int val_type;
+	int val_type;	/* GWBCT_INTEGER, GWBCT_SINGLE, GWBCT_DOUBLE, GWBCT_STRING */
 	GWBC_Value val;
 };
 
@@ -18,7 +18,16 @@ struct GWBR_ExpressionResult {
 	Prototypes
 */
 GWBR_ExpressionResult gwbr_EvaluateExpression(GWBE_Environment* env, GWBN_Expression* node);
+
 GWBR_ExpressionResult gwbr_EvaluateNumericExpression(GWBE_Environment* env, GWBN_NumericExpression* node);
+GWBR_ExpressionResult gwbr_EvaluateArithmeticOperator(GWBE_Environment* env, GWBN_ArithmeticOperator* node);
+GWBR_ExpressionResult gwbr_EvaluateRelationalOperator(GWBE_Environment* env, GWBN_RelationalOperator* node);
+GWBR_ExpressionResult gwbr_EvaluateLogicalOperator(GWBE_Environment* env, GWBN_LogicalOperator* node);
+GWBR_ExpressionResult gwbr_EvaluateFunctionalOperator(GWBE_Environment* env, GWBN_FunctionalOperator* node);
+GWBR_ExpressionResult gwbr_EvaluateNumericTerm(GWBE_Environment* env, GWBN_NumericTerm* node);
+GWBR_ExpressionResult gwbr_EvaluateNumericConstant(GWBE_Environment *env, GWBN_NumericConstant *node);
+GWBR_ExpressionResult gwbr_EvaluateNumericVariable(GWBE_Environment *env, GWBN_NumericVariable *node);
+
 GWBR_ExpressionResult gwbr_EvaluateStringExpression(GWBE_Environment* env, GWBN_StringExpression* node);
 GWBR_ExpressionResult gwbr_EvaluateStringOperator(GWBE_Environment* env, GWBN_StringOperator* node);
 GWBR_ExpressionResult gwbr_EvaluateStringTerm(GWBE_Environment* env, GWBN_StringTerm* node);
