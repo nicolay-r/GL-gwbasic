e0729331fc75dfd2f8bc03f15251d13db63206d7
Nicolay Rusnachenko <kolyarus@yandex.ru>
2015-05-03 09:08:36 +0300

    Дописано приведение типов для разных операций

--- runtime/handlers/interp/expr/relational.c
+++ runtime/handlers/interp/expr/relational.c
@@ -83,18 +83,234 @@ GWBR_ExpressionResult gwbr_EvaluateEqual(GWBR_ExpressionResult a, GWBR_Expressio
 GWBR_ExpressionResult gwbr_EvaluateInequal(GWBR_ExpressionResult a, GWBR_ExpressionResult b)
 {
 	GWBR_ExpressionResult result;
+switch (a.val_type)
+	{
+		case GWBCT_INTEGER:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_INTEGER;
+					result.val.int_val = (a.val.int_val != b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.int_val != b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.int_val != b.val.double_val);
+					break;
+				default:
+					/* error! */
+					break;
+			}
+			break;
+		}
+		case GWBCT_SINGLE:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.single_val != b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.single_val != b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.single_val != b.val.double_val);
+					break;
+				default:
+					/* error */
+					break;
+			}
+			break;		
+		}
+		case GWBCT_DOUBLE:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val != b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val != b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val != b.val.double_val);
+					break;
+				default:
+					/* error */
+					break;
+			}
+			break;
+		}
+		default:
+			/* предусмотреть ошибки */
+			break;
+	}
 
 	return result;
 }
 GWBR_ExpressionResult gwbr_EvaluateGT(GWBR_ExpressionResult a, GWBR_ExpressionResult b)
 {
 	GWBR_ExpressionResult result;
+	switch (a.val_type)
+	{
+		case GWBCT_INTEGER:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_INTEGER;
+					result.val.int_val = (a.val.int_val > b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.int_val > b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.int_val > b.val.double_val);
+					break;
+				default:
+					/* error! */
+					break;
+			}
+			break;
+		}
+		case GWBCT_SINGLE:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.single_val > b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.single_val > b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.single_val > b.val.double_val);
+					break;
+				default:
+					/* error */
+					break;
+			}
+			break;		
+		}
+		case GWBCT_DOUBLE:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val > b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val > b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val > b.val.double_val);
+					break;
+				default:
+					/* error */
+					break;
+			}
+			break;
+		}
+		default:
+			/* предусмотреть ошибки */
+			break;
+	}
 
 	return result;
 }
 GWBR_ExpressionResult gwbr_EvaluateLT(GWBR_ExpressionResult a, GWBR_ExpressionResult b)
 {
 	GWBR_ExpressionResult result;
+	switch (a.val_type)
+	{
+		case GWBCT_INTEGER:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_INTEGER;
+					result.val.int_val = (a.val.int_val < b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.int_val < b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.int_val < b.val.double_val);
+					break;
+				default:
+					/* error! */
+					break;
+			}
+			break;
+		}
+		case GWBCT_SINGLE:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.single_val < b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.single_val < b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.single_val < b.val.double_val);
+					break;
+				default:
+					/* error */
+					break;
+			}
+			break;		
+		}
+		case GWBCT_DOUBLE:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val < b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val < b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val < b.val.double_val);
+					break;
+				default:
+					/* error */
+					break;
+			}
+			break;
+		}
+		default:
+			/* предусмотреть ошибки */
+			break;
+	}
 
 	return result;
 
@@ -102,12 +318,156 @@ GWBR_ExpressionResult gwbr_EvaluateLT(GWBR_ExpressionResult a, GWBR_ExpressionRe
 GWBR_ExpressionResult gwbr_EvaluateGTE(GWBR_ExpressionResult a, GWBR_ExpressionResult b)
 {
 	GWBR_ExpressionResult result;
+	switch (a.val_type)
+	{
+		case GWBCT_INTEGER:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_INTEGER;
+					result.val.int_val = (a.val.int_val >= b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.int_val >= b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.int_val >= b.val.double_val);
+					break;
+				default:
+					/* error! */
+					break;
+			}
+			break;
+		}
+		case GWBCT_SINGLE:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.single_val >= b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.single_val >= b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.single_val >= b.val.double_val);
+					break;
+				default:
+					/* error */
+					break;
+			}
+			break;		
+		}
+		case GWBCT_DOUBLE:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val >= b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val >= b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val >= b.val.double_val);
+					break;
+				default:
+					/* error */
+					break;
+			}
+			break;
+		}
+		default:
+			/* предусмотреть ошибки */
+			break;
+	}
 
 	return result;
 }
 GWBR_ExpressionResult gwbr_EvaluateLTE(GWBR_ExpressionResult a, GWBR_ExpressionResult b)
 {
 	GWBR_ExpressionResult result;
+	switch (a.val_type)
+	{
+		case GWBCT_INTEGER:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_INTEGER;
+					result.val.int_val = (a.val.int_val <= b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.int_val <= b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.int_val <= b.val.double_val);
+					break;
+				default:
+					/* error! */
+					break;
+			}
+			break;
+		}
+		case GWBCT_SINGLE:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.single_val <= b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_SINGLE;
+					result.val.single_val = (a.val.single_val <= b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.single_val <= b.val.double_val);
+					break;
+				default:
+					/* error */
+					break;
+			}
+			break;		
+		}
+		case GWBCT_DOUBLE:
+		{
+			switch (b.val_type)
+			{
+				case GWBCT_INTEGER:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val <= b.val.int_val);
+					break;
+				case GWBCT_SINGLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val <= b.val.single_val);
+					break;
+				case GWBCT_DOUBLE:
+					result.val_type = GWBCT_DOUBLE;
+					result.val.double_val = (a.val.double_val <= b.val.double_val);
+					break;
+				default:
+					/* error */
+					break;
+			}
+			break;
+		}
+		default:
+			/* предусмотреть ошибки */
+			break;
+	}
 
 	return result;
 }
