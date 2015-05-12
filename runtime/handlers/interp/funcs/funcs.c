e266a4554b3f9031f2d8910e64bc57a307232821
Nicolay Rusnachenko <kolyarus@yandex.ru>
2015-05-12 23:27:24 +0300

    Добавлены файлы funcs в раздел runtime. Исправлена ошибка сборки

---
+++ runtime/handlers/interp/funcs/funcs.c
@@ -0,0 +1,39 @@
+/* GWBasic Functions implementation */
+
+#include "inc/funcs.h"
+#include <assert.h>
+
+#include "../expr/inc/eval.h" /* Expression Evaluator */
+#include "../../../inc/output.h"
+
+#include <stdio.h>
+
+GWBR_Result gwbh_Function(GWBE_Environment *env, GWBN_Function* node) {
+	GWBR_Result result;
+	result.type = GWBR_RESULT_OK;
+	
+	assert(env != NULL);
+	assert(node != NULL);
+	assert(node->math_func != NULL);
+
+	gwbo_DisplayMessage(env, "In \"Function\" Handler\n"); 
+	
+	GWBR_ExpressionResult expr_result;
+	switch (node->type)
+	{
+		case GWBNT_MATHFUNCTION:
+			/* вычислить математическое выражение */
+			expr_result = gwbr_EvaluateMathFunction(env, node->math_func);	
+			break;
+		case GWBNT_STRINGFUNCTION:
+		case GWBNT_SYSTEMFUNCTION:
+			break;
+	}
+	/* 
+		можно еще отобразить результат на экран 
+		для этого потребуется ф-ция вывода
+	*/
+	gwbo_DisplayCoreValue(env, &expr_result.val);	
+	return result;	 
+} 
+	
---
+++ runtime/handlers/interp/funcs/inc/funcs.h
@@ -0,0 +1,12 @@
+/* GWBasic Function header for Ast Node handlers */
+
+#ifndef _GWBR_FUNCS_H_
+#define _GWBR_FUNCS_H_
+
+#include "../../../../inc/runtime.h"
+#include "../../../../inc/env.h"
+#include "../../../../inc/parser.h"
+
+GWBR_Result gwbh_Function(GWBE_Environment *env, GWBN_Function* node);
+
+#endif
